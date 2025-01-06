72fad67ad5c2aa339e60c85ea749df7b4d1da0bf
sa1210 <61729905+sa1210@users.noreply.github.com>
2025-01-06 21:21:49 +0900

    Create service-worker.js

---
+++ 22/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
+        '/styles.css', // 必要なスタイルシートやJSファイルもここに追加
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
